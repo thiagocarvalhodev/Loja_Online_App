68c88b5303f292a5f6805e94d7ea2fe42b4bc844
thiagocarvalho0877 <tsc.carvalho@mat.ci.ufpb.br>
2019-03-11 14:41:31 -0300

    Iniciado ProductTile que fara a tile do produto

--- loja_online/lib/screens/category_screen.dart
+++ loja_online/lib/screens/category_screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loja_online/datas/product_data.dart';
+import 'package:loja_online/tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -22,16 +24,38 @@ class CategoryScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            Container(
-                color: Colors.red,
-              ),
-              Container(
-                color: Colors.yellow,
-              )
-          ],
-        ),
+        body: FutureBuilder<QuerySnapshot>(
+          // acessando os itens de cada categoria (selecionada)
+          future: Firestore.instance.collection("products").document(snapshot.documentID)
+          .collection("items").getDocuments(),
+          builder: (context, snapshot){
+            if(!snapshot.hasData)
+              return Center(child: CircularProgressIndicator(),);
+
+            else{
+              return TabBarView(
+                children: <Widget>[
+                  // cria a grade de imagens conforme as imagens vão carregando
+                  GridView.builder(
+                    padding: EdgeInsets.all(4.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                      childAspectRatio: 0.65
+                    ),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index){
+                      // transformamos os dados do firebase em um objeto do tipo ProductData
+                      ProductData productData = ProductData.fromDocument(snapshot.data.documents[index]);
+                      ProductTile("grid", productData);
+                    },
+                  )
+                ],
+              );
+            }
+          },
+        )
       ),
     );
   }
