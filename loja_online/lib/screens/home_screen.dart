661b453f05c68c42b2d9a392d61999de530f4643
thiagocarvalho0877 <tsc.carvalho@mat.ci.ufpb.br>
2019-03-11 10:20:48 -0300

    Iniciada a tela ProductTab, tela que mostra os produtos

--- loja_online/lib/screens/home_screen.dart
+++ loja_online/lib/screens/home_screen.dart
@@ -6,7 +6,7 @@
  * Utilizamos a ordem de cada tela para a navegação das páginas.
  * Utilizamos essa ordem no DrawerTile para navegação
  * 0 - HomeTab()
- * 1 -
+ * 1 - ProductTab()
  * 2 -
  * 3 -
  */
@@ -14,6 +14,8 @@
 import 'package:flutter/material.dart';
 import 'package:loja_online/tabs/home_tab.dart';
 import 'package:loja_online/widgets/custom_drawer.dart';
+import 'package:loja_online/tabs/product_tab.dart';
+
 
 class HomeScreen extends StatelessWidget {
   final Widget child;
@@ -28,10 +30,18 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController, // controlador do PageView
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Scaffold(
+        // Passamos o PageController PageView para o CustomDrawer para podermos
+        // ter acesso ao controle da navegação das páginas
+        Scaffold( // tela '0'
           body: HomeTab(),
-          // Passamos o PageController PageView para o CustomDrawer para podermos
-          // ter acesso ao controle da navegação das páginas
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold( // tela '1'
+          appBar: AppBar(
+            title: Text("Produtos"),
+            backgroundColor: Theme.of(context).primaryColor,
+          ),
+          body: ProductTab(),
           drawer: CustomDrawer(_pageController),
         )
       ],
---
+++ loja_online/lib/tabs/product_tab.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProductTab extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
