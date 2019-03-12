097184caca6e267956ee3e684cb3b59efd065856
thiagocarvalho0877 <tsc.carvalho@mat.ci.ufpb.br>
2019-03-12 11:38:54 -0300

    Iniciada tela de cadastro signUpScreen

---
+++ loja_online/lib/screens/sing_up_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class SingUpScreen extends StatelessWidget {
+  final _formKey =GlobalKey<FormState>();
+
+  final Widget child;
+
+  SingUpScreen({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrar"),
+        centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            onPressed: (){},
+            child: Text("CRIAR CONTA", style: TextStyle(
+              color: Colors.white
+            ),),
+          )
+        ],
+      ),
+      // como iremos usar formulários, colocamos dentro do Form
+      body: Form(
+        key: _formKey, // key do form
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Nome Completo",
+              ),
+              validator: (text){
+                if(text.isEmpty) return "Nome inválido";
+              },
+            ),
+            // email
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "E-mail",
+              ),
+              validator: (text){
+                // caso seja validado, não precisamos retornar
+                // nada
+                if(text.isEmpty || !text.contains("@"))
+                  return "E-mail inválido";
+              }, // função de validação
+            ),
+            // campo de senha
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Senha",
+              ),
+              validator: (text){
+                if(text.length < 6) 
+                  return "Senha inválida";
+              }, // função de validação
+            ),
+
+            TextFormField(  // Campo de endereço
+              decoration: InputDecoration(
+                hintText: "Endereço",
+              ),
+              validator: (text){
+                if(text.isEmpty) 
+                  return "Endereço inválido"; 
+              },
+            ),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                child: Text("Criar Conta", style: TextStyle(
+                  color: Colors.white
+                ),),
+                onPressed: (){
+                  if(_formKey.currentState.validate()){
+                    
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
